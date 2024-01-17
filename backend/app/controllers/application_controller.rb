dd7f268bace14e954d052e471e136497ddd81649
Eng Yu Han <engyuhan@icloud.com>
2024-01-17 21:29:56 +0800

    Created Users Backend
    Created migration CreateUsers, added type UUID to user_id, added unique indices to username and email.
    Created model User, added has_secure_password, added validations for uniqueness, presence, character limits and formats for username and email.
    Created controller users, added signup, signin, refresh actions, and profile show, update and delete actions.
    Updated application controller, added create_token, delete_token and other methods for authentication.
    Updated routes for users controller actions.

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,46 @@
 class ApplicationController < ActionController::API
+    # Get secret key from encrypted credentials file.
+    def jwt_key
+        Rails.application.credentials.jwt_key
+    end
+
+    # Create access jwt with 30 min expiry.
+    def create_access_token(user)
+        JWT.encode({sub: user.id, exp: Time.now.to_i + Constants::ACCESS_VALIDITY}, jwt_key, "HS256")
+    end
+
+    # Create refresh jwt with 30 days expiry.
+    def create_refresh_token(user)
+        JWT.encode({sub: user.id, exp: Time.now.to_i + Constants::REFRESH_VALIDITY}, jwt_key, "HS256")
+    end
+ 
+    # Decode token and check expiry.
+    def decode_token(token)
+        begin
+            decoded_token = JWT.decode(token, jwt_key, true, { algorithm: 'HS256'}).first
+        rescue => exception
+            {error: "Invalid Token"}
+        end
+    end
+
+    def header_token
+        header = request.headers["Authorization"]
+        if !header.nil?
+            header[7..-1]
+        else
+            nil
+        end
+    end
+
+    def user_id
+        decode_token(header_token)["sub"]
+    end
+
+    def current_user
+        user ||= User.find_by(id: user_id)
+    end
+
+    def logged_in?
+        !!current_user
+    end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Users routes.
+  post '/signup', to: 'users#signup'
+  post '/signin', to: 'users#signin'
+  post '/refresh', to: 'users#refresh'
+  get '/profile', to: 'users#show'
+  post '/profile', to: 'users#update'
+  put '/profile', to: 'users#update'
+  delete '/profile', to: 'users#destroy'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
